e2ddf727109b189ab2efe6618f22f74924769e3d
Mike Repec <mikerepec@gmail.com>
2023-10-08 19:14:48 +0200

    Setup user auth via devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, path: 'auth'
+  # devise_scope :users do
+    # get '/signout', to: 'devise/sessions#destroy', as: :signout
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'home#index'
