ce2858a54e5bad8ac196c28af736aa98f1edd0d2
Mike Repec <mikerepec@gmail.com>
2023-10-14 13:11:31 +0200

    Fix rspec linter warnings

--- spec/requests/feeds_spec.rb
+++ spec/requests/feeds_spec.rb
@@ -7,16 +7,24 @@ RSpec.describe 'Feeds' do
     it 'gets redirected to login page' do
       get feeds_path
       expect(response).to have_http_status(:found)
+    end
+
+    it 'gets redirected to login page url' do
+      get feeds_path
       expect(response.headers['Location']).to eq('http://www.example.com/auth/sign_in')
     end
   end
 
   describe 'GET /feeds authenticated' do
-    include_context 'logged in'
+    include_context 'when logged in'
 
     it 'can access feeds page' do
       get feeds_path
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'can see feeds page' do
+      get feeds_path
       expect(response.body).to include('My Feeds')
     end
   end
--- spec/support/shared_contexts.rb
+++ spec/support/shared_contexts.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.shared_context 'logged in' do
+RSpec.shared_context 'when logged in' do
   let(:user_to_sign_in) do
     (defined?(user) && user.presence) || create(:user)
   end
