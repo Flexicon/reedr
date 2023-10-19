960c7398fe4b4c8fac15acb871758e6d5dde7777
Mike Repec <mikerepec@gmail.com>
2023-10-19 22:24:12 +0200

    Tap and update when creating

--- app/context/use_cases/add_new_feed.rb
+++ app/context/use_cases/add_new_feed.rb
@@ -8,24 +8,21 @@ module UseCases
     end
 
     def call
-      return validator unless validator.validate
+      feed = Feed.new(url:, user:)
+      return feed unless feed.validate
 
-      Feed.create(
-        title: fetched_rss.title || 'N/A',
-        subtitle: fetched_rss.subtitle,
-        latest_id: fetched_rss.items.first&.hexdigest,
-        url:,
-        user:
-      )
+      feed.tap do |f|
+        f.update(
+          title: fetched_rss.title || 'N/A',
+          subtitle: fetched_rss.subtitle,
+          latest_id: fetched_rss.items.first&.hexdigest
+        )
+      end
     end
 
     private
 
     attr_reader :url, :user
-
-    def validator
-      @validator ||= Feed.new(url:, user:)
-    end
 
     def fetched_rss
       @fetched_rss ||= RSSFeedFetcher.fetch(url)
