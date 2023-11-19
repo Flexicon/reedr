33ab14d2bd783631fa0f7abca289d02d42800bf5
Mike Repec <mikerepec@gmail.com>
2023-11-19 14:59:56 +0100

    Handle AOS in turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,20 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import AOS from "aos"
-import "controllers"
+import '@hotwired/turbo-rails';
+import AOS from 'aos';
+import 'controllers';
 
-// Init animate on scroll 🪄
-AOS.init({ duration: 600 });
+// Initialize AOS on page load
+document.addEventListener('turbo:load', () => {
+  AOS.init();
+});
+
+// Refresh AOS after Turbo renders new content
+document.addEventListener('turbo:render', () => {
+  Array.from(document.getElementsByClassName('aos-init')).forEach((el) => {
+    el.classList.remove('aos-init', 'aos-animate');
+  });
+
+  setTimeout(() => {
+    AOS.refresh();
+  }, 100);
+});
