452e252b1d8b549ac6be755469a9f8e43a25bbe4
Mike Repec <mikerepec@gmail.com>
2023-11-19 14:02:10 +0100

    Setup aos

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import AOS from "aos"
 import "controllers"
+
+// Init animate on scroll 🪄
+AOS.init({ duration: 600 });
